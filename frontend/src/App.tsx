d1f90010dbd9a418563090b90e0f4984a2ea09a5
Athena Sy Kuzuhara <sykuzuha@usc.edu>
2025-09-21 20:00:02 -0700

    added homepage and nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './homepage';
+import Navbar from './Navbar';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-
   return (
-    <div>
-      <h1>Hi! Team hearing development in progress....yooooo</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/" element={
+          <div style={{marginTop: '84px', padding: '20px'}}>
+            <h1>Hi! Team hearing development in progress....yooooo</h1>
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
