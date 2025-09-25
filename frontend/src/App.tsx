d094d22ccf063e6706a272f699888f2d65c9307e
Isai Rajaraman <irajaram@usc.edu>
2025-09-25 10:50:54 -0700

    Finished profile checking package files as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Homepage from './homepage';
 import Navbar from './Navbar';
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage"; 
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} /> 
         <Route path="/" element={
           <div style={{marginTop: '84px', padding: '20px'}}>
             <h1>Hi! Team hearing development in progress....yooooo</h1>
