835bc1dfa2a5c3f8862fc3c417ee49d8be6bff68
Aarushi Garg <aarushigarg@gmail.com>
2025-09-27 17:39:21 -0700

    Fix paths

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './homepage.css';
+import '../style/Homepage.css';
 
 const Homepage: React.FC = () => {
   const handleButtonClick = (buttonName: string) => {
@@ -9,48 +9,48 @@ const Homepage: React.FC = () => {
 
   return (
     <div className="homepage-container">
-        <div className="button-grid">
-          <div className="top-row">
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Audiometry')}
-            >
-              Audiometry
-            </button>
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Environmental Sounds')}
-            >
-              Environmental Sounds
-            </button>
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Music Exercises')}
-            >
-              Music Exercises
-            </button>
-          </div>
-          <div className="bottom-row">
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Psychophysics')}
-            >
-              Psychophysics
-            </button>
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Seeing and Hearing Speech')}
-            >
-              Seeing and Hearing Speech
-            </button>
-            <button 
-              className="exercise-button" 
-              onClick={() => handleButtonClick('Speech Recognition')}
-            >
-              Speech Recognition
-            </button>
-          </div>
+      <div className="button-grid">
+        <div className="top-row">
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Audiometry')}
+          >
+            Audiometry
+          </button>
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Environmental Sounds')}
+          >
+            Environmental Sounds
+          </button>
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Music Exercises')}
+          >
+            Music Exercises
+          </button>
         </div>
+        <div className="bottom-row">
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Psychophysics')}
+          >
+            Psychophysics
+          </button>
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Seeing and Hearing Speech')}
+          >
+            Seeing and Hearing Speech
+          </button>
+          <button
+            className="exercise-button"
+            onClick={() => handleButtonClick('Speech Recognition')}
+          >
+            Speech Recognition
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import '../style/Index.css';
 import App from './App';
 import React from 'react';
 
