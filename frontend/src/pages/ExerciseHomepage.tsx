3c3a65681ee27165e1cb83410a5f487e707f7ea7
Aarushi Garg <aarushigarg@gmail.com>
2025-10-11 17:05:13 -0700

    Update pitch resolution test to use pitchshifted notes from A2

--- frontend/src/pages/ExerciseHomepage.tsx
+++ frontend/src/pages/ExerciseHomepage.tsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import "../style/MusicExercises.css";
+import React from "react";
 
 const ExerciseHomepage: React.FC = () => {
   const { exerciseName } = useParams<{ exerciseName: string }>();
@@ -25,7 +26,7 @@ const ExerciseHomepage: React.FC = () => {
           onClick={() => navigate("/music-exercises")}
         >
           <svg width="28" height="28" viewBox="0 0 28 28" fill="none">
-            <path d="M18 7L11 14L18 21" stroke="#222" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M18 7L11 14L18 21" stroke="#222" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </button>
         <h2 className="music-exercises-title">
