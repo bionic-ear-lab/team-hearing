5eb0d29418830730d5440ad5de3db590b7febac9
Aarushi Garg <aarushigarg@gmail.com>
2025-10-23 17:41:36 -0700

    Update test to use selected base note

--- frontend/src/pages/PitchResolutionTest.tsx
+++ frontend/src/pages/PitchResolutionTest.tsx
@@ -1,25 +1,31 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import TestCore from "./TestCore";
 import { playNotes } from "../api/notePlayback";
 import { createQuestion, getSemitoneGap } from "../api/noteSelection";
 import { evaluateAnswer, updatePitchIndex } from "../api/userChoice";
 
-const PitchResolutionTest: React.FC = () => (
-  <TestCore
-    testName="Pitch Resolution Test"
-    question="Which note is higher in pitch?"
-    baseNotes={[45, 57, 69, 81, 93]}
-    defaultIndex={34}
-    correctShift={-1}
-    incorrectShift={3}
-    numberOfAttempts={5}
-    buttonOptions={["1", "2"]}
-    questionGenerator={createQuestion}
-    getSemitoneGap={getSemitoneGap}
-    evaluator={evaluateAnswer}
-    indexUpdater={updatePitchIndex}
-    player={playNotes}
-  />
-);
+const PitchResolutionTest: React.FC = () => {
+  const location = useLocation();
+  const userBaseNote = location.state?.baseNote ?? 45; // A2 default if none provided
+
+  return (
+    <TestCore
+      testName="Pitch Resolution Test"
+      question="Which note is higher in pitch?"
+      baseNotes={[userBaseNote]}
+      defaultIndex={34}
+      correctShift={-1}
+      incorrectShift={3}
+      numberOfAttempts={5}
+      buttonOptions={["1", "2"]}
+      questionGenerator={createQuestion}
+      getSemitoneGap={getSemitoneGap}
+      evaluator={evaluateAnswer}
+      indexUpdater={updatePitchIndex}
+      player={playNotes}
+    />
+  );
+};
 
 export default PitchResolutionTest;
