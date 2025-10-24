396c78b39786622d49a979e02bb18d74ae195fe9
Aarushi Garg <aarushigarg@gmail.com>
2025-10-23 17:28:27 -0700

    Fixes + logging. Test works as it should

--- frontend/src/pages/PitchResolutionTest.tsx
+++ frontend/src/pages/PitchResolutionTest.tsx
@@ -1,35 +1,24 @@
 import React from "react";
-import TestCore, { useMusicTest } from "./TestCore";
+import TestCore from "./TestCore";
 import { playNotes } from "../api/notePlayback";
 import { createQuestion, getSemitoneGap } from "../api/noteSelection";
 import { evaluateAnswer, updatePitchIndex } from "../api/userChoice";
 
-const PitchResolutionTest: React.FC = () => {
-  const { testProps, handleAnswer, handleRepeat, handleEndTest, handleStart, handleBack } = useMusicTest({
-    testName: "Pitch Resolution Test",
-    question: "Which note is higher in pitch?",
-    baseNotes: [45, 57, 69, 81, 93],
-    defaultIndex: 34,
-    correctShift: -1,
-    incorrectShift: 3,
-    numberOfAttempts: 5,
-    questionGenerator: createQuestion,
-    getSemitoneGap,
-    evaluator: evaluateAnswer,
-    indexUpdater: updatePitchIndex,
-    player: playNotes,
-  });
-
-  return (
-    <TestCore
-      {...testProps}
-      onStart={handleStart}
-      onRepeat={handleRepeat}
-      onFinish={handleEndTest}
-      onAnswer={handleAnswer}
-      onBack={handleBack}
-    />
-  );
-};
+const PitchResolutionTest: React.FC = () => (
+  <TestCore
+    testName="Pitch Resolution Test"
+    question="Which note is higher in pitch?"
+    baseNotes={[45, 57, 69, 81, 93]}
+    defaultIndex={34}
+    correctShift={-1}
+    incorrectShift={3}
+    numberOfAttempts={5}
+    questionGenerator={createQuestion}
+    getSemitoneGap={getSemitoneGap}
+    evaluator={evaluateAnswer}
+    indexUpdater={updatePitchIndex}
+    player={playNotes}
+  />
+);
 
 export default PitchResolutionTest;
